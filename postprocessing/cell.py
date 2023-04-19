4a6f12233cf1782bee7e9b8b8583737d7e213e39
DejanKov2901 <127941319+DejanKov2901@users.noreply.github.com>
2023-04-19 18:12:57 +0200

    ATP workflow mostly integrated in the other workflow, ATP flag added, script not executable at the moment
    MembraneDetetor no more exists; ATPImageConverter takes care of specialties in ATP images (preparation of image copy for segmentation with stardist; membrane segmentation)

--- postprocessing/cell.py
+++ postprocessing/cell.py
@@ -1,3 +1,5 @@
+from skimage.transform import SimilarityTransform
+from skimage.transform import warp
 
 class CellImage:
     def __init__(self, roi1, roi2):
@@ -5,27 +7,14 @@ class CellImage:
         self.channel2 = roi2
         self.steps_executed = []
         self.ratio = None
-
+        self.cell_image_registrator = CellImageRegistrator()
 
     def channel_registration(self):
-        # TODO: implement channel registration
         print("Here comes the channel registration")
-        # registration of channel 1 and 2
-        pass
-
-    #def execute_processing_step(self, step, parameters):
-    #    if (isinstance(step, MembraneSegmentation)):
-    #        segmentedMembraneMask = step.execute(self.channel1, self.channel2, parameters) # not very handsome code
-    #        self.channel1 = step.applyMembraneMask(self.channel1, segmentedMembraneMask)
-    #        self.channel2 = step.applyMembraneMask(self.channel2, segmentedMembraneMask)
-    #    elif (isinstance(step, RatioCalculation)):
-    #        self.channel1 = step.execute(self.channel1, self.channel2, parameters)
-    #        self.channel2 = step.execute(self.channel1, self.channel2, parameters)
-    #    else:
-    #        self.channel1 = step.execute(self.channel1, parameters)
-    #        self.channel2 = step.execute(self.channel2, parameters)
-
-    #    self.steps_executed.append(step.give_name())
+        x_offset, y_offset = self.cell_image_registrator.measure_mean_offset_optical_flow(self.channel1.return_image(),
+                                                                                          self.channel2.return_image())
+        tform = SimilarityTransform(translation=(-x_offset, -y_offset))
+        self.channel2.image = warp(self.channel2.return_image, tform)
 
     def calculate_ratio(self):
         ratio = self.channel1.return_image()/self.channel2.return_image()
@@ -36,6 +25,11 @@ class CellImage:
 
     def give_image_channel2(self):
         return self.channel2.return_image()
+
+    def execute_processing_step(self, step, parameters):
+        self.channel1 = step.execute(self.channel1, parameters)
+        self.channel2 = step.execute(self.channel2, parameters)
+        self.steps_executed.append(step.give_name())
 
 
 
@@ -55,4 +49,60 @@ class ChannelImage:
 
     def getWavelength (self):
         return self.wavelength
+
+
+from skimage import io
+from skimage import registration
+import numpy as np
+from scipy.ndimage import shift
+from matplotlib import pyplot as plt
+
+
+class CellImageRegistrator:
+    def __init__(self):
+        pass
+
+    def measure_mean_offset_optical_flow (self, channel_1_image, channel_2_image):
+        """
+        Performs optical flow measurement with a reference image and a corresponding offset image.
+
+        :param reference_channel_first_frame: the reference image
+        :param offset_channel_first_frame:  the cell image that has an offset compared to the reference
+        :return xoff:
+        """
+
+        flow = registration.optical_flow_tvl1(channel_1_image, channel_2_image)
+
+        # dense optical flow
+        flow_x = flow[1, :, :]
+        flow_y = flow[0, :, :]
+
+        # find mean shift in x and y direction
+        xoff = np.mean(flow_x)
+        yoff = np.mean(flow_y)
+        print("x offset" + str(xoff))
+        print("y offset" + str(yoff))
+
+        # round offsets
+        xoff = round(xoff)
+        yoff = round(yoff)
+
+        """
+        corrected_image = shift(offset_channel, shift=(-xoff, -yoff), mode='constant')
+        fig = plt.figure(figsize=(10, 10))
+        ax1 = fig.add_subplot(2, 2, 1)
+        ax1.imshow(reference_channel, cmap='gray')
+        ax1.title.set_text('Reference Image')
+        ax2 = fig.add_subplot(2, 2, 2)
+        ax2.imshow(offset_channel, cmap='gray')
+        ax2.title.set_text('Offset image')
+        ax3 = fig.add_subplot(2, 2, 3)
+        ax3.imshow(corrected_image, cmap='gray')
+        ax3.title.set_text('Corrected')
+        plt.show()
+        # print("Pixels shifted by: ", xoff, yoff)
+        """
+
+        return xoff, yoff
+
 
--- postprocessing/segmentation.py
+++ postprocessing/segmentation.py
@@ -1,7 +1,11 @@
 from stardist.models import StarDist2D
 from csbdeep.utils import normalize
-
-
+import numpy as np
+from skimage import filters as filters
+from skimage.morphology import area_closing
+from skimage.morphology import binary_erosion, binary_dilation, remove_small_holes
+from skimage.segmentation import clear_border
+from skimage.util import invert
 
 class SegmentationSD:
     def __init__(self, model='2D_versatile_fluo'):
@@ -19,4 +23,126 @@ class SegmentationSD:
        # coord_list.sort(key=lambda coord_list1: coord_list1[2])
         return output_specs['coord']
 
+class BaseSegmentation:
+    def give_coord_channel1(self, input_image, seg_model):
 
+        # gives list of all coordinates of ROIS in channel1
+        coord_list1 = []
+        seg_img_channel1, output_specs_channel1 = seg_model.predict_instances(normalize(np.hsplit(input_image, 2)[0]),
+                                                                            prob_thresh=0.6, nms_thresh=0.2)
+        if len(output_specs_channel1['coord']) >= 0:
+            for coords in output_specs_channel1['coord']:
+                x_coords = coords[1]
+                y_coords = coords[0]
+                coord_list1.append(list(zip(x_coords, y_coords)))
+        coord_list1.sort(key=lambda coord_list1: coord_list1[2])
+        return coord_list1
+
+    def give_coord_channel2(self, input_image, seg_model):
+
+        # mit offset bestimmen
+        # gives list of all coordinates of ROIS in channel2
+        coord_list2 = []
+        seg_img_channel2, output_specs_channel2 = seg_model.predict_instances(normalize(np.hsplit(input_image, 2)[1]),
+                                                                            prob_thresh=0.6, nms_thresh=0.2)
+        if len(output_specs_channel2['coord']) >= 0:
+            for coords in output_specs_channel2['coord']:
+                x_coords = coords[1]
+                x_coords = [x + float(input_image.shape[1] / 2) for x in x_coords]
+                y_coords = coords[0]
+                coord_list2.append(list(zip(x_coords, y_coords)))
+        coord_list2.sort(key=lambda coord_list2: coord_list2[2])
+        return coord_list2
+
+
+
+class ATPImageConverter:
+    """
+    Converts ATP-sensor images so that they can be processed in the segmentation pipeline.
+    """
+
+    def prepare_ATP_image_for_segmentation(self, img):
+        """
+
+        :param img:
+        :return:
+        """
+        original_image = img.copy()
+        # smoothing and thresholding
+        img = filters.gaussian(img, 2)
+        # triangle for 100x images, li for 63x images
+        img = img < filters.threshold_triangle(img)
+        # img = img < filters.threshold_li(img)
+
+        # remove small holes; practically removing small objects from the inside of the cell membrane
+        # param area_threshold = 1000 for 100x images, 500 for 63x images
+        img = remove_small_holes(img, area_threshold=1000, connectivity=2)
+
+        # invert image so that holes can be properly filled
+        img = invert(img)
+
+        # dilate image to close holes in the membrane
+        number_of_iterations = 4
+        img = self.binary_dilate_n_times(img, number_of_iterations)
+
+        # Fill holes within the cell membranes in the binary image
+        # param area_threshold = 200000 for 100x images, 100000 for 63x images
+        img = area_closing(img, area_threshold=200000, connectivity=2)
+
+        # erode again after dilation (same number of iterations)
+        img = self.binary_erode_n_times(img, number_of_iterations)
+
+        # remove objects on the edge
+        img = clear_border(img)
+
+        # assign the value 255 to all black spots in the image and the value 0 to all white areas
+        mask_positive = img == True
+        mask_negative = img == False
+
+        original_img_masked = original_image.copy()
+        original_img_masked[mask_positive] = 255
+        original_img_masked[mask_negative] = 0
+
+        return original_img_masked
+
+    def binary_erode_n_times(self, img, n):
+        """
+        Returns the binary-eroded input image "img" after n iterations.
+        :param img: the input image
+        :param n: the number of iterations
+        :return: the n-times binary-eroded image
+        """
+        img_eroded = img
+        for x in range(n):
+            img_eroded = binary_erosion(img_eroded)
+        return img_eroded
+
+    def binary_dilate_n_times(self, img, n):
+        """
+        Returns the binary-dilated input image "img" after n iterations.
+        :param img: the input image
+        :param n: the number of iterations
+        :return: the n-times binary-dilated image
+        """
+        img_dilated = img
+        for x in range(n):
+            img_dilated = binary_dilation(img_dilated)
+        return img_dilated
+
+    def segment_membrane_in_ATP_image_pair(self, channel_1_image, channel_2_image):
+        channel1 = channel_1_image.copy()
+        channel2 = channel_2_image.copy()
+
+        gaussian = filters.gaussian(channel_1_image, 1.5)
+        binary_image = gaussian < filters.threshold_li(gaussian)
+
+        # remove small holes; practically removing small objects from the inside of the cell membrane
+        # inverted logic
+        # param area_threshold = 1000 for 100x images, 500 for 63x images
+        small_objects_removed = remove_small_holes(binary_image, area_threshold=1000, connectivity=2)
+        membrane_mask = small_objects_removed == True
+
+        channel1 = self.apply_mask_on_image(channel1, membrane_mask, 0)
+        channel2 = self.apply_mask_on_image(channel2, membrane_mask, 0)
+
+        return channel1, channel2
