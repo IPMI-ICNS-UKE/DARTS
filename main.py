c3225052b781802948a501c5faad6ddf12db4323
Hümeyra Husseini <h.husseini@uke.de>
2023-06-15 17:18:09 +0200

    vorübergehender progress bar für shape normalization, hotspot detection --> bar for subprocesses (rich etc already tested but meh)

--- main.py
+++ main.py
@@ -29,46 +29,61 @@ def main(gui_enabled):
     os.makedirs(savepath, exist_ok=True)
     dartboard_data_multiple_cells = []
 
-    for i, cell in enumerate(Processor.cell_list):
-        ratio = cell.give_ratio_image()
-        try:
-            normalized_ratio, centroid_coords_list = Processor.normalize_cell_shape(cell)
-        except Exception as E:
-            print(E)
-            print("Error in shape normalization")
-            continue
-        cell_image_radius_after_normalization = 50  # provisorisch...
-        io.imsave(savepath + "cellratio" + str(i) + ".tif", ratio)
-        io.imsave(savepath + "cellratio_normalized" + str(i) + ".tif", normalized_ratio)
+    print("\nProcessing now continues with: ")
+    with alive_bar(len(Processor.cell_list), force_tty=True) as bar:
+        for i, cell in enumerate(Processor.cell_list):
+            time.sleep(.005)
+            ratio = cell.give_ratio_image()
+            try:
+                normalized_ratio, centroid_coords_list = Processor.normalize_cell_shape(cell)
+                sh = True
+            except Exception as E:
+                print(E)
+                print("Error in shape normalization")
+                continue
+
+            cell_image_radius_after_normalization = 50  # provisorisch...
+            io.imsave(savepath + "cellratio" + str(i) + ".tif", ratio)
+            io.imsave(savepath + "cellratio_normalized" + str(i) + ".tif", normalized_ratio)
+
+            try:
+                Processor.detect_hotspots(normalized_ratio, cell, i)
+                hd = True
+            except Exception as E:
+                print(E)
+                print("Error in Hotspot Detection")
+                continue
+
+            Processor.save_measurements()
+            try:
+                if (not cell.is_excluded):
+                    dartboard_data_single_cell = Processor.generate_average_dartboard_data_single_cell(
+                        centroid_coords_list, cell, cell_image_radius_after_normalization, i)
+                    dartboard_data_multiple_cells.append(dartboard_data_single_cell)
+            except Exception as E:
+                print(E)
+                print("Error in Dartboard (single cell)")
+                continue
+            bar()
+        time.sleep(.005)
+        if sh == True:
+            print("Shape normalization of: ", i+1, "cell(s)", end='\r')
+        print()
+        if hd == True:
+            print("Hotspot detection of: ", i+1, "cell(s)", end='\r')
+        print()
 
         try:
-            Processor.detect_hotspots(normalized_ratio, cell, i)
+            Processor.generate_average_and_save_dartboard_multiple_cells(dartboard_data_multiple_cells)
+            print("Dartboard plot: Done!")
         except Exception as E:
             print(E)
-            print("Error in Hotspot Detection")
-            continue
+            print("Error in Dartboard (average dartboard for multiple cells)")
 
-        Processor.save_measurements()
-        try:
-            if(not cell.is_excluded):
-                dartboard_data_single_cell = Processor.generate_average_dartboard_data_single_cell(
-                    centroid_coords_list, cell, cell_image_radius_after_normalization, i)
-                dartboard_data_multiple_cells.append(dartboard_data_single_cell)
-        except Exception as E:
-            print(E)
-            print("Error in Dartboard (single cell)")
-            continue
-
-    try:
-        Processor.generate_average_and_save_dartboard_multiple_cells(dartboard_data_multiple_cells)
-    except Exception as E:
-        print(E)
-        print("Error in Dartboard (average dartboard for multiple cells)")
-
-    Processor.save_image_files()  # save processed cropped images
-    Processor.save_ratio_image_files()
-    # fig = Processor.plot_rois()
-    # fig.savefig(save_path_Ca_cAMP + "rois.jpg")
+        Processor.save_image_files()  # save processed cropped images
+        Processor.save_ratio_image_files()
+        # fig = Processor.plot_rois()
+        # fig.savefig(save_path_Ca_cAMP + "rois.jpg")
 
 
 if __name__ == "__main__":
