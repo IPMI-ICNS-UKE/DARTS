85089caf6249b63f4a7d2a716c6c9832e6c7ea68
DejanKov2901 <127941319+DejanKov2901@users.noreply.github.com>
2023-07-28 16:28:13 +0200

    minor corrections

--- postprocessing/CellTracker_ROI.py
+++ postprocessing/CellTracker_ROI.py
@@ -83,7 +83,7 @@ class CellTracker:
         if not features.empty:
             # tp.annotate(features[features.frame == (0)], image_series[0])  # generates a plot
             # tracking, linking of coordinates
-            search_range = 50  # TO DO: needs to be optimised, adaptation to estimated cell diameter/area
+            search_range = 50
             t = tp.link_df(features, search_range, memory=0)
             t = tp.filtering.filter_stubs(t, threshold=number_of_frames-1)
             # print (t)
