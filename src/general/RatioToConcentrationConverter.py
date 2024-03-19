553ea6bb3c3147db24fd0b0e68b6ab819ddd0c8f
DejanKov2901 <127941319+DejanKov2901@users.noreply.github.com>
2024-03-19 17:05:21 +0100

    calibration parameters can be entered in the GUI now and used for the measurement. Not applicable for the "default cell types (NK, jurkat, primary)"

--- src/general/RatioToConcentrationConverter.py
+++ src/general/RatioToConcentrationConverter.py
@@ -2,11 +2,11 @@
 import math
 
 class RatioConverter():
-    def __init__(self):
-        pass
+    def __init__(self, parameter_dict):
+        self.parameter_dict = parameter_dict
 
 
-    def calcium_calibration(self, ratio_mean_value, celltype, spotHeight = 0):
+    def calcium_calibration(self, ratio_value, celltype, spotHeight = 0):
         """
         Returns calcium threshold value for calcium hotspots value based on the provided ratio mean value (ratio_value)
         and cell type.
@@ -14,14 +14,13 @@ class RatioConverter():
         :param celltype: 'primary‘ or 'jurkat' or 'NK'
         :return:
         """
-        concentration = None
 
         if celltype == 'jurkat':
             a = 15.48  # a = 15.48
             b = 1.9  # b = 1.644 -> 2.0 -> 1.9
 
 
-            corresponding_Ca_value = a * math.e**(b*ratio_mean_value)
+            corresponding_Ca_value = a * math.e**(b*ratio_value)
             threshold_Calcium = corresponding_Ca_value + spotHeight
             threshold_ratio = (math.log(threshold_Calcium/a))/b
             return corresponding_Ca_value, threshold_Calcium, threshold_ratio
@@ -37,7 +36,7 @@ class RatioConverter():
             cInv = -1.025
             dInv = -0.004879
 
-            corresponding_Ca_value = a * math.e**(b*ratio_mean_value) + c*math.e**(d*ratio_mean_value)
+            corresponding_Ca_value = a * math.e**(b*ratio_value) + c*math.e**(d*ratio_value)
             threshold_Calcium = corresponding_Ca_value + spotHeight
             threshold_ratio = aInv * math.e**(bInv*threshold_Calcium) + cInv*math.e**(dInv*threshold_Calcium)
             return corresponding_Ca_value, threshold_Calcium, threshold_ratio
@@ -57,11 +56,24 @@ class RatioConverter():
 
             # invFittedFunction = @(y) aInv * exp(bInv * y) + cInv * exp(dInv * y);
 
-            corresponding_Ca_value = a*math.e**(b*ratio_mean_value) + c*math.e**(d*ratio_mean_value)
+            corresponding_Ca_value = a*math.e**(b*ratio_value) + c*math.e**(d*ratio_value)
             threshold_Calcium = corresponding_Ca_value + spotHeight
             threshold_ratio = aInv*math.e**(bInv*threshold_Calcium) + cInv*math.e**(dInv*threshold_Calcium)
             return corresponding_Ca_value, threshold_Calcium, threshold_ratio
+
         else:
-            return None
+
+            kd_value = self.parameter_dict['KD value (of Ca2+ dye)']
+            min_ratio = self.parameter_dict['minimum ratio']
+            max_ratio = self.parameter_dict['maximum ratio']
+            min_intensity = self.parameter_dict['minimum fluorescence intensity']
+            max_intensity = self.parameter_dict['maximum fluorescence intensity']
+            spotHeight = self.parameter_dict['spot Height Ca2+ microdomains']
+
+            corresponding_Ca_value = kd_value*((ratio_value-min_ratio)/(max_ratio-ratio_value))*(min_intensity/max_intensity)
+            threshold_Calcium = corresponding_Ca_value + spotHeight
+            threshold_ratio = (threshold_Calcium * (max_ratio - min_ratio) * max_intensity) / (kd_value * min_intensity + threshold_Calcium * max_intensity) + min_ratio
+
+            return corresponding_Ca_value, threshold_Calcium, threshold_ratio
 
 
