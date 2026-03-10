b5408061943c66577bd0259c3098d185d4b6fb3f
Deckenlampe <FelixWissel@outlook.com>
2026-03-10 11:41:24 +0100

    SpotHeight

--- src/analysis/GUInoBeads_local.py
+++ src/analysis/GUInoBeads_local.py
@@ -39,7 +39,7 @@ class GUInoBeads_local:
         elif self.cell_type == 'NK':
             self.spotHeight = 72  # needs to be checked
         elif self.cell_type == 'NK_human':
-            self.spotHeight = 72  # Platzhalter
+            self.spotHeight = 112.5  # Platzhalter
         self.ratio_converter = ratioconverter
         self.number_of_frames, self.image_height, self.image_width = self.image.shape
         self.GUI_width, self.GUI_height = 1200, 900  # Increased height to prevent overlapping
